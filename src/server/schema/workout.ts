06b901dc738246ac9c2d74ad21f9762cf3c79476
kTrzcinskii <kt.trzcinski@gmail.com>
2022-07-08 21:21:47 +0200

    added error messages to schema

--- src/server/schema/workout.ts
+++ src/server/schema/workout.ts
@@ -10,19 +10,22 @@ export const getSingleWorkoutInput = z.object({
 });
 
 export const createExerciseInput = z.object({
-  title: z.string(),
-  series: z.number(),
-  repsInOneSeries: z.number(),
+  title: z.string().min(1, "Title is required"),
+  series: z.number().min(1),
+  repsInOneSeries: z.number().min(1),
   weight: z.number().optional(),
 });
 
 export type createExerciseSchema = z.TypeOf<typeof createExerciseInput>;
 
 export const createWorkoutInput = z.object({
-  title: z.string(),
-  description: z.string(),
-  breakDuration: z.number().min(0),
-  exercises: z.array(createExerciseInput),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  breakDuration: z
+    .number()
+    .min(0)
+    .or(z.string().min(1, "Break Duration is required")),
+  exercises: z.array(createExerciseInput).min(1),
 });
 
 export type createWorkoutSchema = z.TypeOf<typeof createWorkoutInput>;
